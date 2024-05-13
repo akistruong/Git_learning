f08396dd83e5638bc5ed5f94455c68f0cc7aaf62
Kiet Truong <bearcoding288@gmail.com>
2024-05-13 12:40:56 +0700

    Product model init

---
+++ Backend_GitLerning/Backend_GitLerning/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend_GitLerning.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
